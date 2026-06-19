2ec7a7b84c22a3db322ae0ca184a3c4a58e2d727
Bade Mani <mbade329@gmail.com>
2026-06-19 19:25:02 +0530

    Add basic calculator functions in script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function appendValue(value){
+    document.getElementById("result").value += value;
+}
+
+function clearResult(){
+    document.getElementById("result").value = "";
+}
+
+function calculate(){
+    document.getElementById("result").value =
+    eval(document.getElementById("result").value);
+}
